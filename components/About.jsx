5f0e33c2544a354a612e821a35daea5e8d600e38
prio12 <maksudurprio.mp@gmail.com>
2026-04-15 13:10:25 +0600

    feat: about section added for both mobile and lg and md screen

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,10 @@
 export default function About() {
   return (
-    <div className="px-4 mt-16">
-      <h2 className="text-5xl pl-0 md:pl-15 lg:pl-15 hidden md:block lg:block font-bold text-[#161616]">
+    <div className="px-4 mt-12">
+      <h2
+        className="text-5xl hidden leading-[1.1] tracking-[-0.03em] font-bold text-[#161616] 
+               lg:block md:block max-w-[90%] mb-6 pl-0 md:pl-22"
+      >
         <span className="">Wij maken content die opvalt. Die</span>
         <br />
         <span>blijft hangen. Die jouw doelgroep</span>
@@ -11,7 +14,10 @@ export default function About() {
         <span>brengt. Snel, krachtig en energiek.</span>
         <br />
       </h2>
-      <h2 className="5xl block md:hidden lg:hidden font-bold text-[#161616]">
+      <h2
+        className="text-[28px] leading-[1.1] tracking-[-0.03em] font-bold text-[#161616] 
+               block md:hidden max-w-[90%] mb-6"
+      >
         Wij maken content die opvalt. Die blijft hangen. Die jouw doelgroep
         raakt en jouw merk in beweging brengt. Snel, krachtig en energiek.
       </h2>
